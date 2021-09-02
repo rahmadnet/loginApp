4efbdcaecc87d319d92d58e8d047c0a39c7ed5bc
rahmadnet <rahmadnasution2@gmail.com>
2021-09-02 11:14:35 +0700

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('/app');
+const app = require('./src/app');
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port);
