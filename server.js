239d655eeb8c5b42f4a75f1649c70076d8d6dcff
rahmadnet <rahmadnasution2@gmail.com>
2021-09-02 11:01:54 +0700

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('./src/app');
+const app = require('/app');
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port);
